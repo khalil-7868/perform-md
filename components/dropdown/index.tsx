382b40def6b9359bca2f839b517aef5db1099b27
khalil-7868 <mkhalil7868@gmail.com>
2024-08-27 09:03:33 +0500

    update header and footer for mobile

--- components/dropdown/index.tsx
+++ components/dropdown/index.tsx
@@ -1,6 +1,8 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { cn } from 'lib/utils';
 
@@ -10,14 +12,37 @@ interface Props {
   label: string;
   dropdownItems: {
     label: string;
-    link?: string;
+    link: string;
   }[];
 }
 
+const dropdownVariants = {
+  hidden: { opacity: 0, scale: 0.95 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      ease: 'easeOut',
+      staggerChildren: 0.1, // Add stagger delay between children animations
+      when: 'beforeChildren' // Start children animations after the parent
+    }
+  },
+  exit: { opacity: 0, scale: 0.95, transition: { duration: 0.2, ease: 'easeIn' } }
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 }
+};
+
 export default function Dropdown(props: Props) {
+  const pathName = usePathname();
+  console.log(pathName);
+
   return (
     <Menu>
-      {({ open }) => (
+      {({ open, close }) => (
         <>
           <MenuButton
             className={cn(
@@ -29,31 +54,31 @@ export default function Dropdown(props: Props) {
             <ChevronDown className={cn('size-3 shrink-0 transition-all', { 'rotate-180': open })} />
           </MenuButton>
 
-          <MenuItems
-            transition
-            anchor="bottom start"
-            className="mt-2 w-52 origin-top-right rounded-xl border border-white/5 bg-[#2A333D] p-1 text-sm/6 text-white transition duration-100 ease-out focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
-          >
-            {props.dropdownItems.map((item, index) => (
-              <MenuItem key={index}>
-                <>
-                  {item.link && (
+          <AnimatePresence>
+            <MenuItems className="absolute z-50 mt-2 w-52 origin-top-right rounded-xl border border-white/5 bg-[#2A333D] p-1 text-sm/6 text-white transition duration-100 ease-out focus:outline-none">
+              <motion.div
+                variants={dropdownVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+              >
+                {props.dropdownItems.map((item, index) => (
+                  <MenuItem key={index} as={motion.div} variants={itemVariants}>
                     <Link
+                      onClick={close}
                       href={item.link}
-                      className="group flex w-full items-center gap-2 rounded-lg px-4 py-2 uppercase transition-all hover:bg-primary data-[focus]:bg-primary"
+                      className={cn(
+                        'group flex w-full items-center gap-2 rounded-lg px-4 py-2 uppercase transition-all hover:bg-primary data-[focus]:bg-primary',
+                        pathName === item.link ? 'bg-primary' : ''
+                      )}
                     >
                       {item.label}
                     </Link>
-                  )}
-                  {!item.link && (
-                    <span className="group flex w-full items-center gap-2 rounded-lg px-4 py-2 uppercase transition-all data-[focus]:bg-primary">
-                      {item.label}
-                    </span>
-                  )}
-                </>
-              </MenuItem>
-            ))}
-          </MenuItems>
+                  </MenuItem>
+                ))}
+              </motion.div>
+            </MenuItems>
+          </AnimatePresence>
         </>
       )}
     </Menu>
--- components/sections/header/index.tsx
+++ components/sections/header/index.tsx
@@ -1,20 +1,54 @@
 'use client';
 
+import { CloseButton, Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import { NAV_DATA } from 'constants/data';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ArrowUpRight } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import { cn } from 'lib/utils';
+
 import Dropdown from 'components/dropdown';
 
 import logo from '../../logo.png';
 
+const menuUl = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      delayChildren: 0.2,
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const itemLi = {
+  hidden: { opacity: 0, x: -10 },
+  show: { opacity: 1, x: 0 }
+};
+
 export function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+  useEffect(() => {
+    // Update the body height based on navOpen state
+    document.body.style.height = navOpen ? '100vh' : 'auto';
+    document.body.style.overflow = navOpen ? 'hidden' : 'auto'; // Prevent scrolling when nav is open
+
+    // Clean up when the component unmounts
+    return () => {
+      document.body.style.height = 'auto';
+      document.body.style.overflow = 'auto';
+    };
+  }, [navOpen]);
   const pathName = usePathname();
   return (
-    <header className="relative z-50 lg:static">
+    <header className="relative z-50">
       <div className="flex h-7 items-center justify-center gap-5.5 bg-dark-16181B px-10 lg:h-12.5">
         <div className="md:text-13 flex items-center gap-1 text-[8px] uppercase -tracking-0.26px">
           <p className="font-bold text-white">Give US A CALL</p>
@@ -48,7 +82,10 @@ export function Header() {
                   <Dropdown label={item.label} dropdownItems={item.dropdownItems} />
                 ) : (
                   <Link
-                    className="text-15 uppercase text-white transition-all hover:text-primary"
+                    className={cn(
+                      'text-15 uppercase text-white transition-all hover:text-primary',
+                      { 'text-primary': pathName === item.link }
+                    )}
                     href={item.link}
                   >
                     {item.label}
@@ -75,7 +112,7 @@ export function Header() {
             <ArrowUpRight className="absolute right-3 top-1/2 size-4.5 -translate-y-1/2 2xl:right-8" />
           </Link>
         </div>
-        <button className="ml-auto block lg:hidden">
+        <button className="ml-auto block lg:hidden" onClick={() => setNavOpen(!navOpen)}>
           <svg
             width="26"
             height="14"
@@ -83,63 +120,127 @@ export function Header() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <rect width="26" height="2" fill="white" />
-            <rect y="6" width="26" height="2" fill="white" />
-            <rect y="12" width="26" height="2" fill="white" />
+            <rect
+              width="26"
+              height="2"
+              fill="white"
+              style={{
+                transform: navOpen ? 'rotate(45deg) translate(5px, 5px)' : 'none',
+                transformOrigin: 'center',
+                transition: 'transform 0.3s ease'
+              }}
+            />
+            <rect
+              y="6"
+              width="26"
+              height="2"
+              fill="white"
+              style={{
+                opacity: navOpen ? 0 : 1,
+                transition: 'opacity 0.3s ease'
+              }}
+            />
+            <rect
+              y="12"
+              width="26"
+              height="2"
+              fill="white"
+              style={{
+                transform: navOpen ? 'rotate(-45deg) translate(5px, -5px)' : 'none',
+                transformOrigin: 'center',
+                transition: 'transform 0.3s ease'
+              }}
+            />
           </svg>
         </button>
       </div>
-      <div className="absolute inset-x-0 top-[73px] hidden border-t border-primary bg-[#1E242B] px-4 lg:hidden">
+      <div
+        className={cn(
+          'absolute inset-x-0 top-[73px] overflow-auto border-t border-t-transparent bg-[#1E242B] px-4 transition-all duration-300 lg:hidden',
+          navOpen
+            ? 'pointer-events-auto h-[calc(100vh-73px)] border-primary'
+            : 'pointer-events-none h-0'
+        )}
+      >
         <div className="py-[84px]">
           <ul className="space-y-16 text-center">
             {NAV_DATA.map((item, index) => (
-              <li className="relative z-0" key={index}>
+              <motion.li className="relative z-0" variants={itemLi} key={index}>
                 {item.dropdownItems && (
-                  <>
-                    <div className="flex items-center justify-center gap-1 text-[25px] uppercase text-white">
-                      <span>{item.label}</span>
-                      <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        className="shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M18.0005 10L12.7076 15.2929C12.3171 15.6834 11.6839 15.6834 11.2934 15.2929L6.00049 10"
-                          stroke="white"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                        />
-                      </svg>
-                    </div>
-                    <div className="bg-[#2A333D]">
-                      <div className="pointer-events-none absolute -inset-x-4 -top-6 bottom-0 -z-10 bg-[#2A333D] transition-all duration-200" />
-                      <ul className="space-y-8 pb-11 pt-6">
-                        {item.dropdownItems.map((dropdown, index) => (
-                          <li key={index}>
-                            <Link
-                              href={dropdown.link}
-                              className="text-base leading-none text-white"
-                            >
-                              {dropdown.label}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </>
+                  <Disclosure>
+                    {({ open }) => (
+                      <>
+                        <DisclosureButton
+                          className={cn(
+                            'flex w-full items-center justify-center gap-1 text-[25px] uppercase text-white',
+                            { 'text-primary': open }
+                          )}
+                        >
+                          <span>{item.label}</span>
+                          <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            className={cn('shrink-0 transition-all', { 'rotate-180': open })}
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M18.0005 10L12.7076 15.2929C12.3171 15.6834 11.6839 15.6834 11.2934 15.2929L6.00049 10"
+                              stroke="currentColor"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                            />
+                          </svg>
+                        </DisclosureButton>
+                        <div className="overflow-hidden bg-[#2A333D]">
+                          <AnimatePresence>
+                            {open && (
+                              <DisclosurePanel>
+                                <div className="pointer-events-none absolute -inset-x-4 -top-6 bottom-0 -z-10 origin-top bg-[#2A333D] transition-all duration-700" />
+                                <motion.ul
+                                  variants={menuUl}
+                                  initial="hidden"
+                                  animate="show"
+                                  className="space-y-8 pb-11 pt-6"
+                                >
+                                  {item.dropdownItems.map((dropdown, index) => (
+                                    <motion.li key={index} variants={itemLi}>
+                                      <CloseButton
+                                        as={Link}
+                                        href={dropdown.link}
+                                        onClick={() => setNavOpen(false)}
+                                        className={cn(
+                                          'text-base leading-none',
+                                          pathName === dropdown.link ? 'text-primary' : 'text-white'
+                                        )}
+                                      >
+                                        {dropdown.label}
+                                      </CloseButton>
+                                    </motion.li>
+                                  ))}
+                                </motion.ul>
+                              </DisclosurePanel>
+                            )}
+                          </AnimatePresence>
+                        </div>
+                      </>
+                    )}
+                  </Disclosure>
                 )}
                 {!item.dropdownItems && (
                   <Link
                     href={item.link}
-                    className="flex items-center justify-center gap-1 text-[25px] uppercase text-white"
+                    onClick={() => setNavOpen(false)}
+                    className={cn(
+                      'flex items-center justify-center gap-1 text-[25px] uppercase text-white',
+                      { 'text-primary': pathName === item.link }
+                    )}
                   >
                     {item.label}
                   </Link>
                 )}
-              </li>
+              </motion.li>
             ))}
           </ul>
         </div>
